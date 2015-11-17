0e05233cf1a2a201e3abccd6e4a69b907d2c42c9
MarkWaldron <markewaldron@gmail.com>
2015-11-17 18:28:05 -0500

    Starting testing on Wikistack

---
+++ test/page.spec.js
@@ -0,0 +1,8 @@
+describe('Page Controller Tests', function () {
+    describe('A subcategory', function () {
+        xit('tests something', function () {});
+        xit('tests another aspect of the same thing' , function () {});
+    });
+    describe('A different subcategory', function () {});
+});
+describe('A different category', function () {});
